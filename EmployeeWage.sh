b00b570264ea892f7de124fa8a77f7dcaf732384
7506574455 <sanketlalge09@gmail.com>
2021-06-03 03:28:03 -0700

    Added Emp Switch case

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -5,18 +5,21 @@ isFullTime=2
 wagePerHour=20
 workingHr=0
 random=$((RANDOM%3))
-if [ $random -eq $isPartTime ]
-then
-           echo "Employee Present Part Time"
-           workingHr=8
-elif [ $random -eq $isFullTime ]
-then
-           echo "Employee Present Full Time"
-           workingHr=16
-else
-           echo "Employee is Absent"
-           workingHr=0
-fi
+case $random in
+        $isPartTime)
+                 echo "Employee Present Part Time"
+                 workingHr=8
+                 ;;
+        $isFullTime)
+                 echo "Employee Present Full Time"
+                 workingHr=16
+                 ;;
+        0)
+                 echo "Employee is Absent"
+                 workingHr=0
+                 ;;
+esac
 totalWage=$(($workingHr*$wagePerHour))
 echo "Daily Wage is : " $totalWage
+
 
