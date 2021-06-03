809a2e5f28c3e2429ba8309eb5cff0019a320482
7506574455 <sanketlalge09@gmail.com>
2021-06-03 02:19:40 -0700

    Employee Daily Wage Added

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,9 +1,15 @@
 echo "Welcome to Employee Wage Computation program"
 
+isPresent=1
 random=$((RANDOM%2))
 if [ $random -eq 1 ]
 then
      echo "Employee Present"
+     wagePerHour=20
+     workingHr=8
+     DailyWage=$(($workingHr*$wagePerHour))
 else
      echo "Employee Absent"
+     Dailywage=0
 fi
+echo "Employee Daily Wage is : " $DailyWage
